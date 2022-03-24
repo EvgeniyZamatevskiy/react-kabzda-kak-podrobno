e287b3cb233ea020192e532b337843bb5b440e92
EvgeniyZaM <jerry.zam.999@gmail.com>
2022-03-24 12:56:26 +0300

    lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is APP component
+      <Rating />
+      <Accordion />
     </div>
-  );
+  )
 }
 
-export default App;
+function Rating() {
+  return (
+    <div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+    </div>
+  )
+}
+
+function Accordion() {
+  return (
+    <div>
+      <h3>Меню</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
